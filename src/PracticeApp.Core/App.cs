255a99c6a986fc633c7b8cf699ab571c324c3582
@xavi.molloy <xavi.molloy@gmail.com>
2020-12-12 12:12:29 +0100

    First Commit

---
+++ src/PracticeApp.Core/App.cs
@@ -0,0 +1,19 @@
+﻿using MvvmCross.IoC;
+using MvvmCross.ViewModels;
+using PracticeApp.Core.ViewModels.Home;
+
+namespace PracticeApp.Core
+{
+    public class App : MvxApplication
+    {
+        public override void Initialize()
+        {
+            CreatableTypes()
+                .EndingWith("Service")
+                .AsInterfaces()
+                .RegisterAsLazySingleton();
+
+            RegisterAppStart<HomeViewModel>();
+        }
+    }
+}
---
+++ src/PracticeApp.Core/ViewModels/Home/HomeViewModel.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PracticeApp.Core.ViewModels.Home
+{
+    public class HomeViewModel : BaseViewModel
+    {
+    }
+}
